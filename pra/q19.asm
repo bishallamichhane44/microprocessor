688dc33328c165e05893e554f5d0e7d05b3acc0e
Bishal Lamichhane <86123120+bishallamichhane44@users.noreply.github.com>
2024-03-09 11:29:00 +0545

    Updated

--- pra/q19.asm
+++ pra/q19.asm
@@ -1,23 +1,25 @@
-START: LXI D,8570H
-       LXI H,8560H
-       MVI C,0AH
+;BINARY TO BCD  FROM 8560 TO 8570
+    LXI H,8560H
+    LXI D,8570H
+    MVI C,0AH  
+    MVI B,00H
+
+
+L2: MOV A,M
+    MOV B,A
+    CPI 00H
+    JZ SKIP
+    MVI A,00H
+L1: ANA A         ;TO RESET THE FLAGS 
+    ADI 01H
+    DAA
+    DCR B
+    JNZ L1
+SKIP:STAX D
+    INX H
+    INX D
+    DCR C
+    JNZ L2
+    HLT
     
-NEXT: MOV B,M
-      CALL BCD
-      STAX D
-      INX D
-      INX H
-      DCR C
-      JNZ NEXT
-      HLT 
-    
-BCD: MOV A,M
-     CPI 00H
-     RZ
-     MVI A,00H
-LOOP:ANA A
-	 ADI 01H
-     DAA 
-     DCR B
-     JNZ LOOP
-     RET
+
